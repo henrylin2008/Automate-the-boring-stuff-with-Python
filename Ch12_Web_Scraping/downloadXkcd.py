f60e8c7683ad184d6f91b6a379a6abcb9a95a0a7
hlin <henrylin2008@yahoo.com>
2019-07-30 06:42:02 -0700

    downloadXkcd update

--- Ch12_Web_Scraping/downloadXkcd.py
+++ Ch12_Web_Scraping/downloadXkcd.py
@@ -5,12 +5,25 @@ import requests, os, bs4
 
 url = 'https://xkcd.com'              # starting url
 os.makedirs('xkcd', exist_ok=True)   # store comics in ./xkcd
+
 while not url.endswith('#'):
     # TODO: Download the page.
+    print('Downloading page %s...' % url)
+    res = requests.get(url)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text)
 
     # TODO: Find the URL of the comic image.
-
-    # TODO: Download the image.
+    comicElem = soup.select('#comic img')
+    if comicElem == []:
+        print('Could not find comic image.')
+    else:
+        comicUrl = 'https:' + comicElem[0].get('src')
+        # TODO: Download the image.
+        print('Downloading image %s...' %(comicUrl))
+        res = requests.get(comicUrl)
+        res.raise_for_status()
 
     # TODO: Save the image to ./xkcd.
 
