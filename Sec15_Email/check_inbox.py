7d9c533d94b36dd88a2b1a93ec81f0c154547a5c
hlin <henrylin2008@yahoo.com>
2019-09-16 22:03:17 -0700

    updates

--- Sec15_Email/check_inbox.py
+++ Sec15_Email/check_inbox.py
@@ -3,4 +3,5 @@ import imapclient
 conn = imapclient.IMAPClient('imap.gmail.com', ssl=True) # yes, using ssl encryption
 conn.login('hlin@contextlogic.com', 'wftuyzbmxfurlhco') #("email address", "pw")
 conn.select_folder('INBOX', readonly=True) # Inbox folder (99%), readonly mode (no deleting email)
-UIDs = conn.search(['SINCE 16-SEP-2019'])
+UIDs = conn.search('SINCE 16-SEP-2019')
+rawMessage = conn.fetch()
