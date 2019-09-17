2c14d965f007a3d230641bd2ca5c56da4858a5f1
hlin <henrylin2008@yahoo.com>
2019-09-16 22:06:11 -0700

    update email body

--- Sec15_Email/Sending_email.py
+++ Sec15_Email/Sending_email.py
@@ -4,7 +4,7 @@ conn = smtplib.SMTP('smtp.gmail.com', 587) # object for smtplib
 conn.ehlo()  #start connection
 conn.starttls() #start encryption
 conn.login('hlin@contextlogic.com', 'wftuyzbmxfurlhco') #("email address", "pw")
-conn.sendmail('hlin@wish.com', 'hlin@wish.com', 'Subject: Email test \n\n 2nd test, \n')
+conn.sendmail('hlin@wish.com', 'hlin@wish.com', 'Subject: Email test \n\n @10:05pm \n')
 # {}: mean email sent successfully
 
 # conn.quit()  # disconnect from SMTP server
